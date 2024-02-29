c3afd2f48d31dd53650a7312f91410c44bfc2854
Yann Mandza <yann.mandza@ess.eu>
2024-02-29 16:14:58 +0100

    fixed param test

--- drvAsynIseghalServiceApp/src/drvAsynIseghalService.cpp
+++ drvAsynIseghalServiceApp/src/drvAsynIseghalService.cpp
@@ -125,7 +125,7 @@ drvAsynIseghalService::drvAsynIseghalService( const char *portName, const char *
 		1, // maxAddr
 		NUM_ISEGHAL_SERVICE_PARAMS,
 		// Interface mask
-		asynCommonMask | asynInt32Mask | asynUInt32DigitalMask | asynFloat64Mask | asynDrvUserMask | asynOctetMask,
+		asynCommonMask | asynInt32Mask | asynUInt32DigitalMask | asynFloat64Mask | asynOctetMask,
 		asynCommonMask | asynInt32Mask | asynUInt32DigitalMask | asynFloat64Mask | asynOctetMask,
 		ASYN_CANBLOCK | ASYN_MULTIDEVICE, // asynFlags.
 		autoConnect, // Autoconnect
@@ -141,6 +141,146 @@ drvAsynIseghalService::drvAsynIseghalService( const char *portName, const char *
 	interface_  	= epicsStrDup( interface );
 	deviceModel_	= epicsStrDup( icsCtrtype );
 	self_ = pasynUserSelf;
+
+	createParam( P_ISEGHAL_SERVICE_SYSSTATUS_STRING,					asynParamUInt32Digital,		&P_SysStatus);
+	createParam( P_ISEGHAL_SERVICE_LNSTATUS_STRING,						asynParamUInt32Digital,		&P_LineStatus);
+	createParam( P_ISEGHAL_SERVICE_CRTSTATUS_STRING,					asynParamUInt32Digital,		&P_CrtStatus);
+	createParam( P_ISEGHAL_SERVICE_CRTEVTSTATUS_STRING,				asynParamUInt32Digital,		&P_CrtEvtStatus);
+	createParam( P_ISEGHAL_SERVICE_CRTEVTMASK_STRING,					asynParamUInt32Digital,		&P_CrtEvtMask);
+	createParam( P_ISEGHAL_SERVICE_CRTCTRL_STRING,						asynParamUInt32Digital,		&P_CrtCtrl);
+	createParam( P_ISEGHAL_SERVICE_MODSTATUS_STRING,					asynParamUInt32Digital,		&P_ModStatus);
+	createParam( P_ISEGHAL_SERVICE_MODEVTSTATUS_STRING,				asynParamUInt32Digital,		&P_ModevtStatus);
+	createParam( P_ISEGHAL_SERVICE_MODEVTMASK_STRING,					asynParamUInt32Digital,		&P_ModevtMask);
+	createParam( P_ISEGHAL_SERVICE_MODCTRL_STRING,						asynParamUInt32Digital,		&P_ModCtrl);
+	createParam( P_ISEGHAL_SERVICE_CHANSTATUS_STRING,					asynParamUInt32Digital,		&P_ChanStatus);
+	createParam( P_ISEGHAL_SERVICE_CHANEVTSTATUS_STRING,			asynParamUInt32Digital,		&P_ChanEvtStatus);
+	createParam( P_ISEGHAL_SERVICE_CHANEVTMASK_STRING,				asynParamUInt32Digital,		&P_ChanEvtMask);
+	createParam( P_ISEGHAL_SERVICE_CHANCTRL_STRING,						asynParamUInt32Digital,		&P_ChanCtrl);
+	// System items																																										
+	createParam( P_ISEGHAL_SERVICE_SYSCRTNUM_STRING,					asynParamInt32,						&P_SysCrtNum);
+	createParam( P_ISEGHAL_SERVICE_SYSCRTLIST_STRING,					asynParamInt32,						&P_SysCrtList);
+	createParam( P_ISEGHAL_SERVICE_SYSMODNUM_STRING,					asynParamInt32,						&P_SysModNum);
+	createParam( P_ISEGHAL_SERVICE_SYSCCTR_STRING,						asynParamInt32,						&P_SysCCtr);
+	createParam( P_ISEGHAL_SERVICE_SYSLOGL_STRING,						asynParamInt32,						&P_SysLogL);
+	createParam( P_ISEGHAL_SERVICE_SYSLOGPATH_STRING,					asynParamOctet,						&P_SysLogPath);
+	createParam( P_ISEGHAL_SERVICE_SYSLINMODE_STRING,					asynParamInt32,						&P_SysLinMode);
+	createParam( P_ISEGHAL_SERVICE_SYSSCONFIG_STRING,					asynParamInt32,						&P_SysSConfig);
+	createParam( P_ISEGHAL_SERVICE_SYSETHNAME_STRING,					asynParamOctet,						&P_SysEthName);
+	createParam( P_ISEGHAL_SERVICE_SYSETHADDR_STRING,					asynParamOctet,						&P_SysEthAddr);
+	createParam( P_ISEGHAL_SERVICE_SYSSVERS_STRING,						asynParamOctet,						&P_SysSVers);
+	createParam( P_ISEGHAL_SERVICE_SYSNETTOUT_STRING,					asynParamInt32,						&P_SysNetTout);
+	createParam( P_ISEGHAL_SERVICE_SYSSSNAME_STRING,					asynParamOctet,						&P_SysSSname);
+	// Can Line items
+	createParam( P_ISEGHAL_SERVICE_LNBRATE_STRING,						asynParamInt32,						&P_LnBRate);
+	createParam( P_ISEGHAL_SERVICE_LNMODNUM_STRING,						asynParamInt32,						&P_LnModNum);
+	createParam( P_ISEGHAL_SERVICE_LNCRTNUM_STRING,						asynParamInt32,						&P_LnCrtNum);
+	createParam( P_ISEGHAL_SERVICE_LNCRTLIST_STRING,					asynParamInt32,						&P_LnCrtList);
+	createParam( P_ISEGHAL_SERVICE_LNMODLIST_STRING,					asynParamInt32,						&P_LnModList);
+	createParam( P_ISEGHAL_SERVICE_LNLOGL_STRING,							asynParamInt32,						&P_LnLogL);
+	// Crate items
+	createParam( P_ISEGHAL_SERVICE_CRTSTAT_STRING,						asynParamInt32,						&P_CrtStat);
+	createParam( P_ISEGHAL_SERVICE_CRTALIVE_STRING,						asynParamInt32,						&P_CrtAlive);
+	createParam( P_ISEGHAL_SERVICE_CRTPON_STRING,							asynParamInt32,						&P_CrtPOn);
+	createParam( P_ISEGHAL_SERVICE_CRTFSPEEP_STRING,					asynParamInt32,						&P_CrtFSpeed);
+	createParam( P_ISEGHAL_SERVICE_CRTSN_STRING,							asynParamInt32,						&P_CrtSn);
+	createParam( P_ISEGHAL_SERVICE_CRTDEVC_STRING,						asynParamInt32,						&P_CrtDevC);
+	createParam( P_ISEGHAL_SERVICE_CRTFWRLS_STRING,						asynParamOctet,						&P_CrtFWRls);
+	createParam( P_ISEGHAL_SERVICE_CRTFWNAME_STRING,					asynParamOctet,						&P_CrtFWName);
+	createParam( P_ISEGHAL_SERVICE_CRTTEMP_STRING,						asynParamFloat64,					&P_CrtTemp);
+	createParam( P_ISEGHAL_SERVICE_CRTSUPPLY_STRING,					asynParamFloat64,					&P_CrtSupply);
+	createParam( P_ISEGHAL_SERVICE_CRTARTICLE_STRING,					asynParamOctet,						&P_CrtArticle);
+	// device/module items
+	createParam( P_ISEGHAL_SERVICE_MODSTAT_STRING,						asynParamInt32,						&P_ModStat);
+	createParam( P_ISEGHAL_SERVICE_MODALIVE_STRING,						asynParamInt32,						&P_ModAlive);
+	createParam( P_ISEGHAL_SERVICE_MODSN_STRING,							asynParamInt32,						&P_ModSn);
+	createParam( P_ISEGHAL_SERVICE_MODDEVC_STRING,						asynParamInt32,						&P_ModDevC);
+	createParam( P_ISEGHAL_SERVICE_MODFWRLS_STRING,						asynParamOctet,						&P_ModFWRls);
+	createParam( P_ISEGHAL_SERVICE_MODFWNAME_STRING,					asynParamOctet,						&P_ModFWName);
+	createParam( P_ISEGHAL_SERVICE_MODTEMP_STRING,						asynParamFloat64,					&P_ModTemp);
+	createParam( P_ISEGHAL_SERVICE_MODSUPPLY_STRING,					asynParamFloat64,					&P_ModSupply);
+	createParam( P_ISEGHAL_SERVICE_MODSRATE_STRING,						asynParamInt32,						&P_ModSRate);
+	createParam( P_ISEGHAL_SERVICE_MODDFILTER_STRING,					asynParamInt32,						&P_ModDFilter);
+	createParam( P_ISEGHAL_SERVICE_MODVRAMP_STRING,						asynParamFloat64,					&P_ModVRamp);
+	createParam( P_ISEGHAL_SERVICE_MODCRAMP_STRING,						asynParamFloat64,					&P_ModCRamp);
+	createParam( P_ISEGHAL_SERVICE_MODVLIM_STRING,						asynParamFloat64,					&P_ModVLim);
+	createParam( P_ISEGHAL_SERVICE_MODILIM_STRING,						asynParamFloat64,					&P_ModILim);
+	createParam( P_ISEGHAL_SERVICE_MODARTICLE_STRING,					asynParamOctet,						&P_ModArticle);
+	createParam( P_ISEGHAL_SERVICE_MODDOCLR_STRING,						asynParamInt32,						&P_ModDoClr);
+	createParam( P_ISEGHAL_SERVICE_MODFADJ_STRING,						asynParamInt32,						&P_ModFAdj);
+	createParam( P_ISEGHAL_SERVICE_MODKEN_STRING,							asynParamInt32,						&P_ModKEn);
+	// MICC option
+	createParam( P_ISEGHAL_SERVICE_MODHVOK_STRING,						asynParamOctet,						&P_ModHVOk);
+	// Channel items
+	createParam( P_ISEGHAL_SERVICE_CHANTEMP_STRING, 					asynParamFloat64,					&P_ChanTemp);
+	createParam( P_ISEGHAL_SERVICE_CHANVSET_STRING, 					asynParamFloat64,					&P_ChanVset);
+	createParam( P_ISEGHAL_SERVICE_CHANISET_STRING,						asynParamFloat64,					&P_ChanISet);
+	createParam( P_ISEGHAL_SERVICE_CHANVMOM_STRING,						asynParamFloat64,					&P_ChanVMom);
+	createParam( P_ISEGHAL_SERVICE_CHANIMOM_STRING,						asynParamFloat64,					&P_ChanIMom);
+	createParam( P_ISEGHAL_SERVICE_CHANVBOUNDS_STRING,				asynParamFloat64,					&P_ChanVBounds);
+	createParam( P_ISEGHAL_SERVICE_CHANIBOUNDS_STRING,				asynParamFloat64,					&P_ChanIBounds);
+	createParam( P_ISEGHAL_SERVICE_CHANVNOM_STRING,						asynParamFloat64,					&P_ChanVNom);
+	createParam( P_ISEGHAL_SERVICE_CHANINOM_STRING,						asynParamFloat64,					&P_ChanINom);
+	createParam( P_ISEGHAL_SERVICE_CHANDTRIPACT_STRING,				asynParamInt32,						&P_ChanDTripAct);
+	createParam( P_ISEGHAL_SERVICE_CHANDTRIPTIME_STRING,			asynParamInt32,						&P_ChanDTripTime);
+	createParam( P_ISEGHAL_SERVICE_CHANEXTINACT_STRING,    		asynParamInt32,						&P_ChanExtInAct);
+	createParam( P_ISEGHAL_SERVICE_CHANTEMPTRIP_STRING,				asynParamInt32,						&P_ChanTempTrip);
+	// Option Voltage controlled by temperature (VCT);
+	createParam( P_ISEGHAL_SERVICE_CHANTEMPEXT_STRING,				asynParamFloat64,					&P_ChanTempEXT);
+	createParam( P_ISEGHAL_SERVICE_CHANVCTCOEF_STRING,    		asynParamFloat64,					&P_ChanVctCoef);
+	// Option STACK
+	createParam( P_ISEGHAL_SERVICE_CHANRES_STRING,      			asynParamFloat64,					&P_ChanRes);
+	createParam( P_ISEGHAL_SERVICE_CHANVRAMPPRIO_STRING,   		asynParamInt32,						&P_ChanVRampPrio);
+	createParam( P_ISEGHAL_SERVICE_CHANVBOT_STRING,    				asynParamFloat64,					&P_ChanVBot);
+	// Option Reversible
+	createParam( P_ISEGHAL_SERVICE_CHANOUTMOD_STRING,      		asynParamInt32,						&P_ChanOutMod);
+	createParam( P_ISEGHAL_SERVICE_CHANOUTMODLIST_STRING, 		asynParamInt32,						&P_ChanOutModList);
+	createParam( P_ISEGHAL_SERVICE_CHANOUTPOL_STRING,    			asynParamInt32,						&P_ChanOutPol);
+	createParam( P_ISEGHAL_SERVICE_CHANOUTPOLLIST_STRING,			asynParamInt32,						&P_ChanOutPolList);
+	createParam( P_ISEGHAL_SERVICE_CHANVMOD_STRING,    				asynParamInt32,						&P_ChanVMod);
+	createParam( P_ISEGHAL_SERVICE_CHANVMODLIST_STRING,				asynParamInt32,						&P_ChanVModList);
+	createParam( P_ISEGHAL_SERVICE_CHANCMOD_STRING,						asynParamInt32,						&P_ChanCMod);
+	createParam( P_ISEGHAL_SERVICE_CHANCMODLIST_STRING,				asynParamInt32,						&P_ChanCModList);
+	createParam( P_ISEGHAL_SERVICE_CHANVRAMPSPUP_STRING,			asynParamFloat64,					&P_ChanVRampSpUp);
+	createParam( P_ISEGHAL_SERVICE_CHANVRAMSPDN_STRING,				asynParamFloat64,					&P_ChanVRAMSpDn);
+	createParam( P_ISEGHAL_SERVICE_CHANVRAMPSPMIN_STRING,			asynParamFloat64,					&P_ChanVRampSpMin);
+	createParam( P_ISEGHAL_SERVICE_CHANVRAMPSPMAX_STRING,			asynParamFloat64,					&P_ChanVRampSpMax);
+	createParam( P_ISEGHAL_SERVICE_CHANCRAMPSPUP_STRING,			asynParamFloat64,					&P_ChanCRampSpUp);
+	createParam( P_ISEGHAL_SERVICE_CHANCRAMPSPDN_STRING,   		asynParamFloat64,					&P_ChanCRampSpDn);
+	createParam( P_ISEGHAL_SERVICE_CHANCRAMPSPMIN_STRING,			asynParamFloat64,					&P_ChanCRampSpMin);
+	createParam( P_ISEGHAL_SERVICE_CHANCRAMPSPMAX_STRING,			asynParamFloat64,					&P_ChanCRampSpMax);
+	//Items for 0MPV (W-IE-NE-R); Modules
+	createParam( P_ISEGHAL_SERVICE_WNCHANTVMOM_STRING,				asynParamFloat64,					&P_WnChanTVMom);
+	createParam( P_ISEGHAL_SERVICE_WNCHANVRRATE_STRING,				asynParamFloat64,					&P_WnChanVRRate);
+	createParam( P_ISEGHAL_SERVICE_WNCHANVFRATE_STRING,				asynParamFloat64,					&P_WnChanVFRate);
+	createParam( P_ISEGHAL_SERVICE_WNCHANVLTRIP_STRING,				asynParamFloat64,					&P_WnChanVLTrip);
+	createParam( P_ISEGHAL_SERVICE_WNCHANVTRIP_STRING,				asynParamFloat64,					&P_WnChanVTrip);
+	createParam( P_ISEGHAL_SERVICE_WNCHANTVTRIP_STRING,				asynParamFloat64,					&P_WnChanTVTrip);
+	createParam( P_ISEGHAL_SERVICE_WNCHANVCTRIP_STRING,				asynParamFloat64,					&P_WnChanVCtrIP);
+	createParam( P_ISEGHAL_SERVICE_WNCHANVPTRIP_STRING,				asynParamFloat64,					&P_WnChanVPTrip);
+	createParam( P_ISEGHAL_SERVICE_WNCHANVTRIPMAX_STRING,			asynParamFloat64,					&P_WnChanVTripMax);
+	createParam( P_ISEGHAL_SERVICE_WNCHANTVTRIPMAX_STRING,		asynParamFloat64,					&P_WnChanTVTripMax);
+	createParam( P_ISEGHAL_SERVICE_WNCHANCTRIPMAX_STRING,			asynParamFloat64,					&P_WnChanCTripMax);
+	createParam( P_ISEGHAL_SERVICE_WNCHANTEMPTRIPMAX_STRING,	asynParamFloat64,					&P_WnChanTTripMax);
+	createParam( P_ISEGHAL_SERVICE_WNCHANPTRIPMAX_STRING,			asynParamFloat64,					&P_WnChanPTripMax);
+	createParam( P_ISEGHAL_SERVICE_WNCHANVLTRIPTIME_STRING,		asynParamInt32,						&P_WnChanVLTripTime);
+	createParam( P_ISEGHAL_SERVICE_WNCHANVTRIPTIME_STRING,		asynParamInt32,						&P_WnChanVTripTime);
+	createParam( P_ISEGHAL_SERVICE_WNCHANTVTRIPTIME_STRING,		asynParamInt32,						&P_WnChanTVTripTime);
+	createParam( P_ISEGHAL_SERVICE_WNCHANCTRIPTIME_STRING,		asynParamInt32,						&P_WnChanCtrIPTime);
+	createParam( P_ISEGHAL_SERVICE_WNCHANTEMPTRIPTIME_STRING,	asynParamInt32,						&P_WnChanTempTripTime);
+	createParam( P_ISEGHAL_SERVICE_WNCHANPTRIPTIME_STRING,		asynParamInt32,						&P_WnChanPTripTime);
+	createParam( P_ISEGHAL_SERVICE_WNCHANTOUTRIPTIME_STRING,	asynParamInt32,						&P_WnChanTOutripTime);
+	createParam( P_ISEGHAL_SERVICE_WNCHANGROUP_STRING,				asynParamInt32,						&P_WnChanGROUp);
+	createParam( P_ISEGHAL_SERVICE_WNCHANNAME_STRING,					asynParamOctet,						&P_WnChanName);
+	createParam( P_ISEGHAL_SERVICE_WNCHANTRIPACT_STRING,			asynParamInt32,						&P_WnChanTripAct);
+	createParam( P_ISEGHAL_SERVICE_WNCHANVLTRIPACT_STRING,		asynParamInt32,						&P_WnChanVLTripAct);
+	createParam( P_ISEGHAL_SERVICE_WNCHANVTRIPACT_STRING,			asynParamInt32,						&P_WnChanVTripAct);
+	createParam( P_ISEGHAL_SERVICE_WNCHANTVTRIPACT_STRING,		asynParamInt32,						&P_WnChanTVTripAct);
+	createParam( P_ISEGHAL_SERVICE_WNCHANCTRIPACT_STRING,			asynParamInt32,						&P_WnChanCtrIPAct);
+	createParam( P_ISEGHAL_SERVICE_WNCHANTEMPTRIPACT_STRING,	asynParamInt32,						&P_WnChanTempTripAct);
+	createParam( P_ISEGHAL_SERVICE_WNCHANPTRIPACT_STRING,			asynParamInt32,						&P_WnChanPTripAct);
+	createParam( P_ISEGHAL_SERVICE_WNCHANTOUTTRIPACT_STRING,	asynParamInt32,						&P_WnChanTOutTripAct);
+	createParam( P_ISEGHAL_SERVICE_WNCHANUSRCONFIG_STRING,		asynParamInt32,						&P_WnChanUsRConfig);
+
 
 	/* Register the shutdown function for epicsAtExit */
 	epicsAtExit(iseghalSessionShutdown, (void*)this);
@@ -419,139 +559,10 @@ static const char *skipWhite(const char *pstart, int underscoreOk){
 }
 
 int drvAsynIseghalService::hasIsegHalItem (const char *item) {
-	std::vector< std::string >::iterator it;
+/* 	std::vector< std::string >::iterator it;
 	it = std::find( validIsegHalItems.begin(), validIsegHalItems.end(), std::string(item) );
-  if( it != validIsegHalItems.end() ) return true;
+  if( it != validIsegHalItems.end() ) return true; */
 	return false;
-}
-
-asynStatus drvAsynIseghalService::drvUserCreate(asynUser *pasynUser, const char *drvInfo, const char **pptypeName, size_t *psize ) {
-
-	static const char *functionName = "drvUserCreate";
-
-  /* the parameter is of format TYPE_item($(ADDR)) where Type is INT for int, DBL for double, or STR for string and DIG for UINT32DIGITAL
-  * The fully qualified name for setting a new item value is item.ADDR, this is used to create the corresponding isegitem parameter.
-	*/
-  if (strlen(drvInfo) > 4 ) {
-
-    size_t len;
-    const char *p;
-    const char *pnext;
-
-    pnext = skipWhite(drvInfo,0);
-		p = pnext;
-
-		for(len=0; *p && (*p!='_') && (*p!=' '); len++, p++){}
-		if(*p==0) {
-        epicsSnprintf(pasynUser->errorMessage,pasynUser->errorMessageSize,
-            "invalid USERPARAM Must be TYPE _/ item(ADDR)");
-        return asynError;
-		}
-		char paramtype[len+1];
-		paramtype[len] = 0;
-		strncpy(paramtype, pnext, len);
-
-		//next is item
-		pnext = skipWhite(p,1);
-		p = pnext;
-		if(*pnext==0) {
-        epicsSnprintf(pasynUser->errorMessage,pasynUser->errorMessageSize,
-            "invalid USERPARAM Must be TYPE _/ item(ADDR)");
-        return asynError;
-		}
-
-		for(len=0; *pnext && (*pnext!='('); len++, pnext++){}
-
-		if(*pnext==0) {
-        epicsSnprintf(pasynUser->errorMessage,pasynUser->errorMessageSize,
-            "invalid USERPARAM Must be TYPE _/ item(ADDR)");
-        return asynError;
-		}
-		char iseghalItem[len+1];
-		iseghalItem[len] = 0;
-		strncpy(iseghalItem, p, len);
-
-		//next is addr
-		pnext = strstr(p,"(");
-		if(!pnext) {
-			  epicsSnprintf(pasynUser->errorMessage,pasynUser->errorMessageSize,
-            "invalid USERPARAM Must be TYPE _/ item(ADDR)");
-        return asynError;
-		}
-		pnext++;
-		pnext = skipWhite(pnext,0);
-		p = pnext;
-		for(len=0; *p && (*p!=' ') && (*p!=')'); len++, p++){}
-		if(*p==0) {
-        epicsSnprintf(pasynUser->errorMessage,pasynUser->errorMessageSize,
-            "invalid USERPARAM Must be TYPE _/ item(ADDR)");
-        return asynError;
-		}
-
-		char iseghalItemAddr[len+1];
-		iseghalItemAddr[len] = 0;
-		strncpy(iseghalItemAddr, pnext, len);
-
-			asynPrint( pasynUser, ASYN_TRACE_ERROR,
-             "Fully Qualifid iseg Object: %s :%s :%s\n", paramtype, iseghalItem, iseghalItemAddr );
-
-    // Check we have this iseghal item
-		if (!this->hasIsegHalItem(iseghalItem)) {
-            asynPrint(pasynUser, ASYN_TRACE_ERROR,
-                        "%s:%s: Parameter '%s' doesn't exist on iseghal item list\n",
-                        driverName, functionName, iseghalItem);
-      return asynError;
-    }
-		//dont forget the dot between property and address
-		len = strlen(iseghalItem) + strlen(iseghalItemAddr) + 2;
-		char fullyQualifyName[len];
-
-		epicsSnprintf(fullyQualifyName, len+1, "%s.%s", iseghalItemAddr, iseghalItem);
-		//0.0.1.VoltageSet
-
-		int index;
-		if (findParam(fullyQualifyName, &index) ) {
-      // Check we have allocated enough space
-      if (itemIndex > NITEMS) {
-        asynPrint(pasynUser, ASYN_TRACE_ERROR,
-                        "%s:%s: Not enough space allocated to store all iseghal items, increase NITEMS\n",
-                        deviceSession_, functionName);
-            return asynError;
-      }
-
-			/* Make parameter of the correct type*/
-
-			if( strcmp(paramtype, "INT") == 0) {
-				createParam(fullyQualifyName, asynParamInt32, &(this->iseghalItems[itemIndex]));
-				pasynUser->reason = itemIndex;
-				isegHalItemsLookup.insert( std::make_pair( std::string(iseghalItem) ,         itemIndex ) );
-
-			} else if (strcmp(paramtype, "DBL") == 0) {
-				std::cout << "\033[0;33m " << "( " << __FUNCTION__ << " ) from " << epicsThreadGetNameSelf() << " thread: " << "DBL Reason: "<<iseghalItems[itemIndex] << "\033[0m" << std::endl;
-  
-				createParam(fullyQualifyName, asynParamFloat64, &(this->iseghalItems[itemIndex]));
-				pasynUser->reason = itemIndex;
-			} else if (strcmp(paramtype, "STR") == 0) {
-
-				createParam(fullyQualifyName, asynParamOctet, &(this->iseghalItems[itemIndex]));
-			} else if (strcmp(paramtype, "DIG") == 0) {
-				
-				std::cout << "\033[0;33m " << "( " << __FUNCTION__ << " ) from " << epicsThreadGetNameSelf() << " thread: " << " DIG Reason: "<<iseghalItems[itemIndex]<< "\033[0m" << std::endl;
-  
-				createParam(fullyQualifyName, asynParamUInt32Digital, &(this->iseghalItems[itemIndex]));
-				pasynUser->reason = itemIndex;
-			} else {
-
-				asynPrint(this->pasynUserSelf, ASYN_TRACE_ERROR,
-                    "%s:%s: Expected Type is INT|DBL|STR|DIG. Got '%s'\n", driverName, functionName, paramtype);
-        return asynError;
-			}
-			std::cout << "\033[0;33m " << "( " << __FUNCTION__ << " ) from " << epicsThreadGetNameSelf() << " thread: " << "item index: " << itemIndex<<"\033[0m" << std::endl;
-			itemIndex++;
-		}
-	}
-
-	return asynSuccess;
 }
 
 /**
