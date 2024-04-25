dcccdeb14a903c294692e9702dd571c264e35486
Yann Mandza <yann.mandza@ess.eu>
2024-04-25 17:36:59 +0200

    Removing Prod IOC startup script, adding sample startup

--- iocBoot/iocdrvAsynIseghalService/sample.cmd
+++ iocBoot/iocdrvAsynIseghalService/sample.cmd
@@ -3,29 +3,28 @@
 < envPaths
 
 
-#device connection parameters
+#- Device connection parameters
 epicsEnvSet(HAL_PORT, "1454")
 epicsEnvSet(USER, "user")
 epicsEnvSet(PASSWORD, "pass")
 epicsEnvSet(IFACE, "can0")
-epicsEnvSet(HAL_SERVER, "hal://ts2-cm-antenna-10kv-hvps.cslab.esss.lu.se")
+epicsEnvSet(HAL_SERVER, "hal://X.X.X.X")
 
 epicsEnvSet(ISEGSESSION, "ETHHAL")
 epicsEnvSet(ISEGIFACE, "$(HAL_SERVER):$(HAL_PORT)/$(IFACE),$(USER),$(PASSWORD)")
 
 
-## Register all support components
+#- Register all support components
 dbLoadDatabase "$(TOP)/dbd/drvAsynIseghalService.dbd"
 drvAsynIseghalServiceTest_registerRecordDeviceDriver pdbbase
 
-
-## Load ISEGHAL service driver
+#- Load ISEGHAL service driver
 drvAsynIseghalServiceConfig( "$(ISEGSESSION)", "$(ISEGIFACE)", "icsmini", 1 )
 
 epicsThreadSleep(2)
 
 asynSetTraceMask("$(ISEGSESSION)", 0, ERROR|FLOW|DRIVER|WARNING)
-#asynSetTraceMask("$(ISEGSESSION)", 0, ERROR)
+
 dbLoadRecords("../../db/drvAsynIseghalService.db","P=iseghal,R=service,PORT=$(ISEGSESSION), ADDR=0.0.0")
 
 
