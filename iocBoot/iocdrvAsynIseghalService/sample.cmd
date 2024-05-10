2f7873f009df9413799c1fc19385f5593841637b
Yann Mandza <yann.mandza@ess.eu>
2024-05-10 20:15:11 +0200

    Updated sample startup script. adding poller thread 'drvIsegHalPollerThreadSetOpt' calls for timing control

--- iocBoot/iocdrvAsynIseghalService/sample.cmd
+++ iocBoot/iocdrvAsynIseghalService/sample.cmd
@@ -3,7 +3,7 @@
 < envPaths
 
 # Module based systems
-epicsEnvSet(MICC, "")
+epicsEnvSet(ICSMINI, "")
 #- CC24/CC23/CC44/CC43 only
 #-epicsEnvSet(CCXX, "")
 
@@ -26,13 +26,11 @@ epicsEnvSet(AUTOCNNECT, "1")
 #- Load ISEGHAL service driver
 drvAsynIsegHalServiceConfig( "$(ISEGSESSION)", "$(ISEGIFACE)", "$(iCSMODEL)", $(AUTOCNNECT) )
 
-epicsThreadSleep(1)
-
 #asynSetTraceMask("$(ISEGSESSION)", 0, ERROR|FLOW|DRIVER|WARNING|DEVICE)
 
 dbLoadRecords("../../db/system-items.template","P=DRVASYNISEGHAL,R=,PORT=$(ISEGSESSION)")
 dbLoadRecords("../../db/can-items.template","P=DRVASYNISEGHAL,R=,PORT=$(ISEGSESSION)")
-$(MICC=#-)dbLoadRecords("../../db/module-items.template","P=DRVASYNISEGHAL,R=,PORT=$(ISEGSESSION)")
+$(ICSMINI=#-)dbLoadRecords("../../db/module-items.template","P=DRVASYNISEGHAL,R=,PORT=$(ISEGSESSION)")
 $(CCXX=#-)dbLoadRecords("../../db/crate-items.template","P=DRVASYNISEGHAL,R=,PORT=$(ISEGSESSION)")
 dbLoadRecords("../../db/channel-items.template","P=DRVASYNISEGHAL,R=,PORT=$(ISEGSESSION)")
 
@@ -40,3 +38,8 @@ dbLoadRecords("../../db/channel-items.template","P=DRVASYNISEGHAL,R=,PORT=$(ISEG
 
 cd ${TOP}/iocBoot/${IOC}
 iocInit
+epicsThreadSleep(1)
+#- Poller Thread Run at 20Hz
+drvIsegHalPollerThreadSetOpt("Interval", "0.05")
+#- Poller Thread interrupt queueRequest delay, adjust this accordingly.
+drvIsegHalPollerThreadSetOpt("RequestInterval", "0.005")
