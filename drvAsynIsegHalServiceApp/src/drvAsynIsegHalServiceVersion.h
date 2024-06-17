2ceac41ddcdc77149a78db25463482e2c8c36423
Yann Mandza <yann.mandza@ess.eu>
2024-06-17 12:08:08 +0200

    Module versioning header

---
+++ drvAsynIsegHalServiceApp/src/drvAsynIsegHalServiceVersion.h
@@ -0,0 +1,10 @@
+/* Generated file, do not edit! */
+
+/* Version determined from Git */
+
+#ifndef drvAsynIsegHalServiceVERSION
+  #define drvAsynIsegHalServiceVERSION "v3.5.1"
+#endif
+#ifndef drvAsynIsegHalServiceVERSION_DATE
+  #define drvAsynIsegHalServiceVERSION_DATE "Git: 2024-06-16 21:44:04 +0200"
+#endif
